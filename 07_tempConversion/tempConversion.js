3de81b23bf85d36e07d55ace69d7a2e2808cbee8
dm1cael <167719716+dm1cael@users.noreply.github.com>
2024-07-10 18:02:00 -0300

    complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  const conversion = (temperature - 32) * 5/9;
+  if(conversion % 1 != 0) return Number(conversion.toFixed(1));
+  
+  return conversion;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  const conversion = (temperature * 9/5) + 32;
+  if(conversion % 1 != 0) return Number(conversion.toFixed(1));
+  
+  return conversion;
 };
 
 // Do not edit below this line

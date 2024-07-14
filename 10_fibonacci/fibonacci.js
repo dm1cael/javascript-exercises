ee39e3a98021afeceaf4f5807b35757bda157455
dm1cael <167719716+dm1cael@users.noreply.github.com>
2024-07-14 19:01:06 -0300

    finish fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    const formattedIndex = Number(index);
+    if(formattedIndex < 0) return 'OOPS'
+    if(formattedIndex == 0 || formattedIndex == 1) return formattedIndex;
 
+    let firstNum = 0;
+    let secondNum = 1;
+    let nextNum = 0;
+    let hold = 0;
+
+    for(let i = 0; i < index; i++) {
+        if(i != 0) {
+            nextNum = firstNum + secondNum;
+            hold = secondNum;
+            
+            secondNum = nextNum;
+            firstNum = hold;
+        }
+    }
+
+    return nextNum;
 };
 
 // Do not edit below this line

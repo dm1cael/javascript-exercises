a41a6813e360755b35e2c1c019bc73dc34edb886
dm1cael <167719716+dm1cael@users.noreply.github.com>
2024-07-14 18:31:08 -0300

    finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const wantedAlpha = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const formattedWord = Array.from(word.toLowerCase())
+                               .filter((alpha) => wantedAlpha.includes(alpha))
+                               .join('');
+
+    const reverseWord = formattedWord.split('').reverse().join('');
+
+    return formattedWord == reverseWord;
 };
 
 // Do not edit below this line

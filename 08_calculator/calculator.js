8b0f9046b3c27f11425a8a5b022f9bc80d63efe4
dm1cael <167719716+dm1cael@users.noreply.github.com>
2024-07-14 17:02:17 -0300

    finish calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(n) {
+	let sum = 0;
+  
+  n.forEach((number) => {
+    sum += number;
+  });
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  let mult = 1;
+  
+  a.forEach((number) => {
+    mult *= number;
+  })
 
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a == 0) return 1;
+  let factorial = 1;
+
+  for(let i = a; i >= 2; i--) {
+    factorial *= i;
+  }
+
+  return factorial
 };
 
 // Do not edit below this line

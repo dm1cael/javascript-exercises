286aa2403a715e25671785876cc0b80c29dabf39
dm1cael <167719716+dm1cael@users.noreply.github.com>
2024-07-20 18:29:53 -0300

    complete findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    const currentYear = new Date().getFullYear();
 
+    if(death == undefined) return currentYear - birth;
+    return death - birth;
+}
+
+const findTheOldest = function(array) {
+    return array.reduce((previous, current) => {
+        let previousAge = getAge(previous.yearOfBirth, previous.yearOfDeath);
+        let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+        if(currentAge > previousAge) return current;
+        return previous;
+    });
 };
 
 // Do not edit below this line
